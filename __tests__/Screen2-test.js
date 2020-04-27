995b7aa962a74919f2fbff34d12dcf22a9271fbe
Kuldeep Singh <kuldeep.singh@qed42.com>
2020-04-27 18:43:40 +0530

    Unit Testing integrated but still some issues needs to be resolved

--- __tests__/Screen2-test.js
+++ __tests__/Screen2-test.js
@@ -1,18 +1,14 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
-import App from '../App';
 import Screen2 from '../src/components/OnboardingScreens/Screen2';
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+describe('Screen2 testing', () => {
+  it('renders correctly', () => {
+    renderer.create(<Screen2 />);
+  });
 
-it('renders correctly', () => {
-  renderer.create(<Screen2 />);
-});
+})
+
