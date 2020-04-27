07df28ab0734a481654a6742bf43954968b5617a
Kuldeep Singh <kuldeep.singh@qed42.com>
2020-04-27 18:56:30 +0530

    Added 2 more onboarding screens

---
+++ src/components/OnboardingScreens/Screen4.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Screen4 = () => {
+  return (
+    <View>
+      <Text>
+        This is screen 4
+      </Text>
+    </View>
+  )
+}
+
+export default Screen4
